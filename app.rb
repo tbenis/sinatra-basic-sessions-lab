ba43e4ba5abbae9d857ab1260ef9ffff0f8bc437
Benis Tambe <will.jenifer@yahoo.com>
2021-04-23 21:19:23 -0400

    commit tree 49abff4ae5ef15ab8a5e686a0831c1a870785210

--- app.rb
+++ app.rb
@@ -1,4 +1,19 @@
 require_relative 'config/environment'
 
 class App < Sinatra::Base
+
+    configure do 
+        enable :sessions
+        set :session_secret, "this_is_the_secrete"
+    end
+
+    get '/' do
+        erb :index
+    end
+
+    post '/checkout' do
+        @session = session
+        @session[:item] = params[:item]
+        erb :checkout
+    end
 end
